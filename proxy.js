581308bb1a5affb3e7feaf273501001d467cf453
Tilerphy <346410840@163.com>
2017-03-08 13:34:50 +0800

    add mode 2

--- proxy.js
+++ proxy.js
@@ -35,6 +35,38 @@ var proxy = function(){
 			
 		                });
 				break;
+			case 2:
+				return new Promise((resolve, reject)=>{
+					var http = require("http");
+					var xt = require("./template");
+					var instance = new xt();
+					var path = instance.fullfil(template["path"], parameters["path"]).result;
+					var postData = instance.fullfil(template["data"], parameters["data"]).result;
+					var opt = {
+						host:endpoint,
+						method: method,
+						path: path,
+						headers:{
+							'Content-Type': 'application/x-www-form-urlencoded',
+      							'Content-Length': postData.length
+						}
+					};
+					var result = "";
+					var req = http.request(opt, (res)=>{
+						res.on("data", (data)=>{
+							result += data.toString();
+						}).on("end", ()=>{
+							resolve(result);
+						});
+					}).on("error", (err)=>{
+						console.log(err.message);
+						reject(err);
+					});
+					req.write(postData);
+					req.end();
+			
+				});
+				break;
 			default:
 				break;
 		}
@@ -45,14 +77,32 @@ var proxy = function(){
 		var express = require("express");
 		var app = express();
 		var server = http.createServer(app);
-		app.use("/", (req, res, next)=>{
+		app.use("/1", (req, res, next)=>{
 			console.log(req.path);
 			
-			this.send(1, "get", "www.psnine.com", "/{group}{id}",{group:req.query["group"], id:(req.query["id"])?("/"+req.query["id"]):""})
+			this.send(1, "get",req.query["site"], req.query["template"],req.query["paramters"])
 			.then((result)=>{
 				res.end(result);
 			});	
 		
+			
+		});
+
+		app.use("/2", (req, res, next)=>{
+
+			console.log(req.path);
+			var postData = "";
+			req.on("data", (data)=>{
+				postData += data.toString();
+			});
+			req.on("end", ()=>{
+				var postDataInstance = JSON.parse(postData);
+				this.send(2, "post", req.query["site"], postDataInstance["template"], postDataInstance["parameters"])
+				.then((result)=>{
+					res.end(result);
+				});
+
+			});
 			
 		});
 		app.listen(port);
